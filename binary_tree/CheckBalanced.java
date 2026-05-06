fb39e84d88dca73cca5890bbbadc878d32179e11
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2026-05-06 16:33:32 +0600

    balance check in BT

--- binary_tree/CheckBalanced.java
+++ binary_tree/CheckBalanced.java
@@ -6,14 +6,20 @@ public class CheckBalanced {
         root.right = new Node(11);
         root.left =  new Node(9);
         root.left.left = new Node(8);
-
+        // root.left.left.left = new Node(10);
+        System.out.println(checkBalanced(root));
         
     }
 
     public static boolean checkBalanced(Node root){
-        if(root!=null){
+        if(root==null){
             return true;
         }
-        
+        int lh = FindHeight.height(root.left);
+        int rh = FindHeight.height(root.right);
+        if(Math.abs(lh-rh)>1){
+            return false;
+        }
+        return checkBalanced(root.left) && checkBalanced(root.right);
     }
 }
